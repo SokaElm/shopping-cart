2dad2c5a448d83ce1602f06792a36a0340795a8f
selmahi.mail@gmail.com <Soukaina@air-de-loic.home>
2025-05-01 22:04:50 +0200

    adding Cart page

--- src/cart.jsx
+++ src/cart.jsx
@@ -1,39 +1,56 @@
-import { useState, useEffect } from "react";
-import { ShoppingCart } from "lucide-react";
 import styles from "./cart.module.css";
 import { useOutletContext } from "react-router-dom";
 import { Trash2 } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function Cart() {
   const { cartItems } = useOutletContext();
 
+  const totalPrice = cartItems.reduce(
+    (total, item) => total + item.quantity * item.price,
+    0
+  );
+
   const listItems = cartItems.map((product) => (
     <li className={styles.productList} key={product.id} id={product.id}>
-      <img
-        className={styles.productImg}
-        src={product.image}
-        alt={product.title}
-        width="60"
-        height="60"
-      />
-      <p>{product.title}</p>
-      <p> {product.price}$</p>
-      <p> {product.quantity}$</p>
-      <Trash2 onClick={() => addToCart(product)} />
+      <div className={styles.productImg}>
+        <img
+          className={styles.productImg}
+          src={product.image}
+          alt={product.title}
+          width="60"
+          height="60"
+        />
+        <p>{product.title}</p>
+      </div>
+
+      <div className={styles.productInfo}>
+        <p> {product.price}$</p>
+        <p> {product.quantity * product.price}$</p>
+        <Trash2 onClick={() => addToCart(product)} />
+      </div>
     </li>
   ));
 
   return (
-    <>
-      <h1>Cart</h1>
-      <div className="tableHeaders">
-        <h2>Product</h2>
-        <h2>Price</h2>
-        <h2>Quantity</h2>
-        <h2>Total</h2>
+    <div className={styles.content}>
+      <h1 className={styles.mainTitle}>Cart</h1>
+      <div className={styles.tableHeaders}>
+        <h2 className={styles.tableTitle}>Product</h2>
+        <div className={styles.priceHeaders}>
+          <h2 className={styles.tableTitle}>Price</h2>
+          <h2 className={styles.tableTitle}>Quantity</h2>
+          <h2 className={styles.tableTitle}>Total</h2>
+        </div>
       </div>
       <ul className={styles.productGrid}>{listItems}</ul>
-    </>
+      <h3 className={styles.total}>
+        Total: {totalPrice}$
+        <Link to="/account">
+          <button className={styles.btn}>Checkout</button>
+        </Link>
+      </h3>
+    </div>
   );
 }
 export default Cart;
